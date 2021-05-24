73b20a820ec71ac4cda4ea6b01cc8d40bd757c2f
syldysnya <syldysnya@gmail.com>
2021-05-24 01:40:31 -0400

    test3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -16,10 +16,10 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-mongoose;
-//   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("Connected to MongoDB successfully"))
-//   .catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!!"));
 
